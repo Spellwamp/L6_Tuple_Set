39191e415be35a52dfff11686c00da3e792692ff
Spellwamp <44416431+Spellwamp@users.noreply.github.com>
2019-04-07 22:57:30 +0300

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,13 @@
+new_l = [123, 2, 1, 'ad', 'asd', 'asd', '|\|']
+l_w = []
+l_n = []
+l_e = []
+
+for i in new_l:
+    if type(i) == int:
+        l_n.append(i)
+    elif ord(i[0]) > 64 and ord(i[0]) < 91 or ord(i[0]) > 96 and ord(i[0]) < 123:
+        l_w.append(i)
+    else: l_e.append(i)
+
+print(l_w, l_n, l_e, end='\n')
