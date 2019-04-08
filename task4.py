ec2ccd74a64b083a5fff4b361e9e264340a0191c
Spellwamp <44416431+Spellwamp@users.noreply.github.com>
2019-04-08 23:43:31 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,10 +1,15 @@
+"""
+ввести з клавіатури почергово цифру, літеру чи спецсимвол і
+повернути відповідно індекс входження у відповідний кортеж;
+"""
+
 new_l = ('1', '2', '1', 'a', 's', 's', ',')
 new_t = []
 for i in range(len(new_l)):
-    new_t.append(ord(new_l[i]))
-a, b, c = str(input()), str(input()), str(input())
-a = new_t.index(ord(a))
-b = new_t.index(ord(b))
-c = new_t.index(ord(c))
+    new_t.append(new_l[i])
+a, b, c = input(), input(), input()
+a = new_t.index(a)
+b = new_t.index(b)
+c = new_t.index(c)
 
 print(a, b, c)
