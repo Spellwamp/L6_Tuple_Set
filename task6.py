f27f7ec93f81048a1cee23b665935015723c28cf
Spellwamp <44416431+Spellwamp@users.noreply.github.com>
2019-04-08 23:52:14 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,12 +1,29 @@
+"""
+Задано n рядочків символів. Розробити програму, яка визначає і друкує окремо
+приголосні та голосні малі літери латинського алфавіту, які є в кожному рядку.
+"""
 tuples = ('asdweh', 'ouyuqnv', 'zasbvry')
-finaly = []
+final = []
 for i in range(len(tuples)):
     for j in range(len(tuples[i])):
         if tuples[i][j] in 'aeiouy':
-            finaly.append(tuples[i][j])
+            final.append(tuples[i][j])
             # print(tuples[i][j], end='')
         else: print(tuples[i][j], end=' ')
-finaly = tuple(finaly)
+final = tuple(final)
 print()
-for i in range(len(finaly)):
-    print(finaly[i], end=' ')
+for i in range(len(final)):
+    print(final[i], end=' ')
+
+# final = []
+# for i in range(len(tuples)):
+#     for j in range(len(tuples[i])):
+#         if tuples[i][j] in 'aeiouy':
+#             final.append(tuples[i][j])
+#             continue
+            print(tuples[i][j], end='')
+        # print(tuples[i][j], end=' ')
+# final = tuple(final)
+# print()
+# for i in range(len(final)):
+#     print(final[i], end=' ')
