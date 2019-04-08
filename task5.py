d30aa4070c16562bef1a3cef88007e72428f8cde
Spellwamp <44416431+Spellwamp@users.noreply.github.com>
2019-04-08 23:46:40 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,6 +1,12 @@
+"""
+відобразити реверс одного з кортежів.
+"""
 new_l = (123, 2, 1, 'a', 's', 's', ',')
 
 o = -1
 for i in range(len(new_l)):
     print(new_l[o], end=' ')
     o -= 1
+# new_l = list(new_l)
+# new_l.reverse()
+# print(new_l)
