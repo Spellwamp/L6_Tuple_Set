8f54f49c7b42bfc723ce42d56c8d4c9ef3d61eed
Spellwamp <44416431+Spellwamp@users.noreply.github.com>
2019-04-09 11:52:05 +0300

    Create task10.py

---
+++ task10.py
@@ -0,0 +1,21 @@
+"""
+Задано два символьних рядка із малих і великих латинських літер та цифр.
+
+Розробити програму, яка будує і друкує в алфавітному порядку множину літер, які є
+в обох масивах, і множини літер окремо першого і другого масивів.
+"""
+
+my_list = ('a', 's', 'b', 'b', '2', 'A', '1')
+my_list2 = ('z', 's', 'd', '2', '3', 'h', 'A')
+res_1 = set()
+res_2 = set()
+
+for i in range(len(my_list)):
+    if my_list[i] in my_list2 and not 47 < ord(my_list[i]) < 58:
+        res_1.add(my_list[i])
+    elif my_list[i] not in my_list2 and not 47 < ord(my_list[i]) < 58: res_2.add(my_list[i])
+res_l = list(res_1)
+res_l_2 = list(res_2)
+res_l.sort()
+res_l_2.sort()
+print(res_l, res_l_2)
